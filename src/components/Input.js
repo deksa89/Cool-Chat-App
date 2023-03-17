f9833fa0851d470b46d0be9b46610aa6797217e7
Dean Hudek <hudek.dean@gmail.com>
2023-03-17 16:16:19 +0100

    added emojis

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,60 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// const Input = (props) => {
+//   const [text, setText] = useState("");
+
+//   const onChange = (e) => {
+//     setText(e.target.value);
+//   }
+
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+//     setText("");
+
+//     props.onSendChatMessage(text);
+//   }
+
+//   return (
+//     <div className="input">
+//       <form className="message-form" onSubmit={onSubmit}>
+//         <input
+//           onChange={onChange}
+//           value={text}
+//           type="text"
+//           placeholder="Start chatting ..."
+//           autoFocus={true}
+//         />
+//         <button>Send</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default Input;
+
+///////////////////////////////////////////////////////////////
+
+import React, { useState } from 'react';
+import Picker from 'emoji-picker-react';
 
 const Input = (props) => {
   const [text, setText] = useState("");
+  const [showPicker, setShowPicker] = useState(false);
 
   const onChange = (e) => {
     setText(e.target.value);
-  }
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
     setText("");
 
     props.onSendChatMessage(text);
-  }
+  };
+
+  const handleEmojiClick = (emojiObject) => {
+    setText((prevMsg)=> prevMsg + emojiObject.emoji)
+  };
 
   return (
     <div className="input">
@@ -24,10 +66,18 @@ const Input = (props) => {
           placeholder="Start chatting ..."
           autoFocus={true}
         />
+        <img
+          className="emoji-icon"
+          alt='emoji'
+          src="https://icons.getbootstrap.com/assets/icons/emoji-smile.svg"
+          onClick={() => setShowPicker(val => !val)} />
         <button>Send</button>
       </form>
+      <div className='pop-up'>
+        {showPicker && (<Picker onEmojiClick={handleEmojiClick} />)}
+      </div>
     </div>
   );
-}
+};
 
 export default Input;
